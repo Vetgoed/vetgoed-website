8d154e1eece9273071b74c70c028c9f5216d9d4f
Julius van der Vaart <studio@juliusvaart.com>
2016-04-14 12:29:44 +0200

    v2 test

--- js/main.js
+++ js/main.js
@@ -1,27 +1,41 @@
-
 jQuery( document ).ready(function( $ ) {
 
-  $('#animation-wrapper').hover(
-    function() {
-      TweenMax.to($('#idee-gloeilamp'), 0.1, {scale:1.2, opacity: 0});
-      TweenMax.set($('#vetgoed-idee-gloeilamp'), {opacity: 0, scale: 1.2});
-      TweenMax.to($('#vetgoed-idee-gloeilamp'), 0.1, {opacity: 1, scale:1});
 
-      TweenMax.set($('#straal'), {scale:0.5, opacity: 0});
-      TweenMax.to($('#straal'), 0.4, {scale:1, opacity: 1, ease: Power3.easeOut});
+    // Kroketlamp animation
+/*
+        $('#animation-wrapper').hover(
+            function() {
+              TweenMax.to($('#idee-gloeilamp'), 0.1, {scale:1.2, opacity: 0});
+              TweenMax.set($('#vetgoed-idee-gloeilamp'), {opacity: 0, scale: 1.2});
+              TweenMax.to($('#vetgoed-idee-gloeilamp'), 0.1, {opacity: 1, scale:1});
 
-      TweenMax.to($('.vetgoed-idee'), 0.1, {color:"#cecece"});
-      TweenMax.to($('.vetgoed-title'), 0.1, {color:"#000"});
-    }, function() {
+              TweenMax.set($('#straal'), {scale:0.5, opacity: 0});
+              TweenMax.to($('#straal'), 0.4, {scale:1, opacity: 1, ease: Power3.easeOut});
 
-      TweenMax.to($('#idee-gloeilamp'), 0.1, {scale:1, opacity: 1});
-      TweenMax.to($('#vetgoed-idee-gloeilamp'), 0.1, {opacity: 0, scale: 1.2});
+              TweenMax.to($('.vetgoed-idee'), 0.1, {color:"#cecece"});
+              TweenMax.to($('.vetgoed-title'), 0.1, {color:"#000"});
+            }, function() {
 
-      TweenMax.to($('.vetgoed-idee'), 0.1, {color:"#000"});
-      TweenMax.to($('.vetgoed-title'), 0.1, {color:"#cecece"});
+              TweenMax.to($('#idee-gloeilamp'), 0.1, {scale:1, opacity: 1});
+              TweenMax.to($('#vetgoed-idee-gloeilamp'), 0.1, {opacity: 0, scale: 1.2});
 
-      TweenMax.to($('#straal'), 0.1, {scale:0.5, opacity: 0});
-    }
-  );
+              TweenMax.to($('.vetgoed-idee'), 0.1, {color:"#000"});
+              TweenMax.to($('.vetgoed-title'), 0.1, {color:"#cecece"});
+
+              TweenMax.to($('#straal'), 0.1, {scale:0.5, opacity: 0});
+            }
+        );
+*/
+    //
+
+
+    // Random video background
+        var vids = ['vetgoed-webbg1', 'vetgoed-webbg2', 'vetgoed-webbg3', 'vetgoed-webbg4'];
+
+        $('#page').vide('/vid/' + vids[Math.floor(Math.random() * vids.length)], {
+          bgColor: '#000000'
+        });
+    //
+
 
 });
